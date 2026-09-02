06fbd328b39e6d7877a12564fa864e8bd8cbc039
NazimFilzer <nazimfilzer@gmail.com>
2026-09-02 23:03:32 +0530

    Initial Commit

---
+++ Skeval Timer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Skeval Timer
+//
+//  Created by Nazim Filzer on 02/09/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Skeval Timer/Skeval_TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Skeval_TimerApp.swift
+//  Skeval Timer
+//
+//  Created by Nazim Filzer on 02/09/26.
+//
+
+import SwiftUI
+
+@main
+struct Skeval_TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
